6a77c71423613621108e7c1bda896a53c1e99312
Mike M <mike.d.meier@gmail.com>
2012-06-16 10:32:06 -0600

    initial bulk update

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,120 +1,67 @@
-# PMS plugin framework
-import datetime,re
+VIDEO_PREFIX	=	"/video/adultswim"
+NAME          	=	L('Title')
 
+FEED_URL		= 	"http://video.adultswim.com/adultswimdynamic/asfix-svc/episodeSearch/getAllEpisodes?sortByEpisodeRanking=DESC&categoryName=&filterByEpisodeType=PRE,EPI&filterByCollectionId=&filterByAuthType=true&networkName=AS"
+EPISODE_LIST	= 	"http://video.adultswim.com/adultswimdynamic/asfix-svc/episodeSearch/getAllEpisodes?sortByDate=DESC&filterByEpisodeType=PRE,EPI&filterByCollectionId=%s&filterByAuthType=true&networkName=AS" #limit=0&offset=0&
 
-##################################################################################################AS
-VIDEO_PREFIX     = "/video/adultswim"
-NAME          = L('Title')
-
-AS_URL                     = "http://www.adultswim.com"
-AS_VIDURL                     = "http://video.adultswim.com"
-AS_FULL_EPISODES_SHOW_LIST = "http://video.adultswim.com/episodes/index.html"
-
-AS_FEED                    = "http://www.adultswim.com/"
-DEBUG                       = False
-ASart                      ="art-default.jpg"
-ASthumb                    ="icon-default.jpg"
+ART             =	"art-default.jpg"
+ICON	       	=	"icon-default.png"
 
 ####################################################################################################
-
 def Start():
-  Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, '[adult swim]',ASthumb,ASart)
-  Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-  Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
-  
-  MediaContainer.art        =R(ASart)
-  MediaContainer.title1     = NAME
-  DirectoryItem.thumb       =R(ASthumb)
+	Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, '[adult swim]', ICON, ART)
+	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
 
-####################################################################################################
-#def VideoMainMenu():
-#    dir = MediaContainer(mediaType='video') 
-#    dir.Append(Function(DirectoryItem(all_shows, "All Shows"), pageUrl = AS_FULL_EPISODES_SHOW_LIST))
-#    return dir
-    
+	ObjectContainer.art        = R(ART)
+	ObjectContainer.title1     = NAME
+	DirectoryObject.thumb      = R(ICON)
+
 ####################################################################################################
 def VideoMainMenu():
-    pageUrl=AS_FULL_EPISODES_SHOW_LIST
-    dir = MediaContainer(viewGroup="List")
-    content1 = XML.ElementFromURL(pageUrl, isHTML="True")
-    showMap = dict()
-    shownum=0
-    for item in content1.xpath('//div[@id="adultswim-episodes"]'):
-        Log(item)
-        Log(item.findall('./div/dl/dd/a'))
-        things = item.findall('./div/dl/dd/a')
-        
-        for thingies in things:
-          shownum =shownum+1
-          showID=shownum
-          Log(thingies)
-          title=thingies.text
-          titleUrl=thingies.get('href')
-          titleUrl=AS_VIDURL + titleUrl
-          thumb=ASthumb
-          Log(titleUrl)
-          Log(title)
-      #  Log(thumb)
-          showList = showMap.get(title)
-          if showList == None:
-		      showList = []
-		      showMap[title] = showList
-		  # Tuple order here matters
-          showList.append((showID,titleUrl, thumb, title))
-          Log(showList)
-    shows=showMap.keys()
-    Log(shows)
-    shows.sort()
-    Log(shows)
-    shownum=0
-    #for shownames in shows:
-    #  shownum=shownum+1
-    #  showList.append((shownum,shownames.titleUrl,shownames.thumb, shownames.title))
-    for showkey in shows:
-      Log(showkey)
-      for show in showMap[showkey]:
-        Log(show)
-        title=show[3]
-        url=show[1]
-        thumb=show[2]
-        Log("Show: " + title + " | link: " + url)
-        dir.Append(Function(DirectoryItem(showxml, title=title), pageUrl = url))
-      
-    
-    return dir 
-####################################################################################################
-
+	oc = ObjectContainer()
+	data = XML.ElementFromURL(FEED_URL)
+	show_ids = []
+	for episode in data.xpath('//episode'):
+		showId = episode.get('showId')
+		showName = episode.get('collectionTitle')
+		if showId in show_ids:
+			continue
+		else:
+			oc.add(DirectoryObject(key=callback(ShowMenu, showName=showName, showId=showId), title=shownName))
+			show_ids.append(showId)
+	oc.objects.sort(key = lambda obj: obj.title)
+	return oc
 
 ####################################################################################################
-def showxml(sender, pageUrl):
-  dir = MediaContainer(title2=sender.itemTitle, viewGroup="InfoList", noCache=True)
-  content = XML.ElementFromURL(pageUrl, isHTML="True")
-  showID=content.xpath('//head/meta[@name="collectionId"]')[0].get('content')
-  Log(showID)
-  link="http://www.adultswim.com/adultswimdynamic/asfix-svc/episodeSearch/getAllEpisodes?limit=15&offset=0&sortByDate=DESC&categoryName=Comedy&filterByEpisodeType=PRE,EPI,CLI&filterByCollectionId=" + showID + "&networkName=AS"
-  shows=XML.ElementFromURL(link).xpath('//episodes/episode')
-  Log(shows)
-  for show in shows:
-    Log(show)
-    #episodeID  
-    epID=show.get('episodeNumber')
-
-    #title
-    title=show.get('title')
-
-    #thumb
-    thumb=show.get('thumbnailUrl')
-    #summary
-    summary=show.xpath('./description')[0].text
-    #link
-    clip=show.xpath('./episodeLink')[0].get('episodeUrl') 
-    
-    if(int(epID)< 100):
-      title="Episode: " + title
-    if(int(epID)> 100):
-      title="Clip: " + title
-
-    Log("epID: " + epID + " | title: " + title + " | thumb: " + thumb + " | Description: " + summary)
-    dir.Append(WebVideoItem(clip, title=title, thumb=thumb, summary=summary))
-
-  return dir
+def ShowMenu(showName, showId):
+	oc = ObjectContainer(title2=showName)
+	ep_list = XML.ElementFromURL(EPISODE_LIST % showId)
+	for episode in ep_list.xpath('//episode'):
+		title = episode.get('title')
+		show = episode.get('collectionTitle')
+		epIndex = episode.get('subEpisodeNumber')
+		season = episode.get('epiSeasonNumber')
+		try:
+		    epIndex = int(epIndex)
+		    absIndex = None
+		except:
+		    epIndex = episode.get('episodeNumber')
+		    epIndex = int(epIndex.partition(season)[2])
+		season = int(season)
+		summary = episode.xpath('./description')[0].text
+		thumb = episode.get('thumbnailUrl')
+		content_rating = episode.get('rating')
+		duration = episode.get('duration')
+		try:
+		    duration = int(duration)
+		except:
+		    duration = None
+		date = episode.get('originalPremiereDate')
+		try:
+		    date = Datetime.ParseDate(date)
+		except:
+		    date = None
+		episodeUrl = episode.xpath('./episoeLink')[0].get('episodeUrl')
+		oc.add(EpisodeObject(url=episodeUrl, title=title, show=show, index=epIndex, season=season, summary=summary,
+			content_rating=content_rating, duration=duration, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+	return oc
