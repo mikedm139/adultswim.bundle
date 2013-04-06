2b5ce8f8595c52f8f6a7749ed3e45f400d89a018
Mike M <mike.d.meier@gmail.com>
2013-04-06 14:41:17 -0600

    tidy up and add handler/route

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,22 +1,16 @@
-VIDEO_PREFIX	=	"/video/adultswim"
-NAME          	=	L('Title')
 
 FEED_URL		= 	"http://video.adultswim.com/adultswimdynamic/asfix-svc/episodeSearch/getAllEpisodes?sortByEpisodeRanking=DESC&categoryName=&filterByEpisodeType=PRE,EPI&filterByCollectionId=&filterByAuthType=true&networkName=AS"
 EPISODE_LIST	= 	"http://video.adultswim.com/adultswimdynamic/asfix-svc/episodeSearch/getAllEpisodes?sortByDate=DESC&filterByEpisodeType=PRE,EPI&filterByCollectionId=%s&filterByAuthType=true&networkName=AS" #limit=0&offset=0&
 
-ART             =	"art-default.jpg"
-ICON	       	=	"icon-default.png"
-
 ####################################################################################################
 def Start():
-	Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, '[adult swim]', ICON, ART)
-	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-
-	ObjectContainer.art        = R(ART)
-	ObjectContainer.title1     = NAME
-	DirectoryObject.thumb      = R(ICON)
+	ObjectContainer.title1 = '[adult swim]'
+	DirectoryObject.thumb = R('icon-default.png')
+	DirectoryObject.art = R('art-default.jpg')
+	EpisodeObject.art = R('art-default.jpg')
 
 ####################################################################################################
+@handler('/video/adultswim', '[adult swim]')
 def VideoMainMenu():
 	oc = ObjectContainer()
 	data = XML.ElementFromURL(FEED_URL)
@@ -33,6 +27,7 @@ def VideoMainMenu():
 	return oc
 
 ####################################################################################################
+@route('/video/adultswim/shows')
 def ShowMenu(showName, showId):
 	oc = ObjectContainer(title2=showName)
 	ep_list = XML.ElementFromURL(EPISODE_LIST % showId)
@@ -64,5 +59,5 @@ def ShowMenu(showName, showId):
 		    date = None
 		episodeUrl = episode.xpath('./episodeLink')[0].get('episodeUrl')
 		oc.add(EpisodeObject(url=episodeUrl, title=title, show=show, index=epIndex, season=season, summary=summary,
-			content_rating=content_rating, duration=duration, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+			content_rating=content_rating, duration=duration, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=R('icon-default.png'))))
 	return oc
